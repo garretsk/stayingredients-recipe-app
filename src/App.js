8ce2b5c6bf53185790f5cc5ffadf9667980ebd51
Garret Katayama <garret.katayama@gmail.com>
2022-06-25 13:35:56 -0600

    MessagePopUp refactored to expect a span as props.alert

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ if (localStorage.jwtToken) {
 }
 
 const HEROKU_SERVER_MESSAGE = <span>Our Heroku server is starting. StayInGredients was created as part of an undergraduate capstone. Due to budgetary contraints, we used Vercel to deploy our frontend and <a href="https://devcenter.heroku.com/articles/free-dyno-hours">'free dyno hours'</a> from Heroku to deploy our backend. In order to save dyno hours, the dyno sleeps until it receives traffic, which wakes it up. It returns to sleep after a period of inactivity. While our server is starting, you may notice any operations that involve requests to the backend being delayed. Performance will return to normal levels after the server wakes.</span>;
-const SERVER_AWAKE_MESSAGE = <span>"Our Heroku server is now up and running!"</span>;
+const SERVER_AWAKE_MESSAGE = <span>Our Heroku server is now up and running!</span>;
 
 class App extends Component {
     constructor(props) {
