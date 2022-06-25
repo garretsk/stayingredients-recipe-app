e9833cbc0a332664d027b8305e654728c97fb2ce
Garret Katayama <garret.katayama@gmail.com>
2022-06-24 20:51:00 -0600

    MessagePopUp added to replace Heroku server alert

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import PrivateRoute from "./components/private-route/PrivateRoute";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
-import MessagePopUp from "./MessagePopUp";
+import MessagePopUp from "./components/MessagePopUp";
 
 import { Provider } from "react-redux";
 import store from "./store";
